22c77a1b8394490a63a66bd12b1ae6881df4e3a9
Dev Appatam <devappatam@gmail.com>
2025-07-29 11:26:09 +0200

    update app.config.ts for modify preprod link using nrgok to local add slash

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@
 export const workSpaceEnvironnement: 'dev' | 'preprod' | 'prod' = 'preprod';
 
 const BASE_URL: Record<'prod' | 'dev' | 'preprod', string> = {
-  preprod: 'https://b7ca112edc25.ngrok-free.app',
+  preprod: 'https://b7ca112edc25.ngrok-free.app/',
   prod: 'https://api.valdeli.com/',
   dev: `http://localhost:5000/`,
 };
